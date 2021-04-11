7f064270367bf0f9d9cefa6a35adccae422f514c
Vittorio Lo Mele <vittorio@mrbackslash.it>
2021-04-11 15:39:50 +0200

    rough current weather data implementation

--- src/components/MapActivity/MapActivity.js
+++ src/components/MapActivity/MapActivity.js
@@ -22,29 +22,58 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet'; 
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet';
 import './MapActivity.css';
-import {StationMarker} from './StationMarker';
+import { StationMarker } from './StationMarker';
+import * as appSettings from '../../settings';
 
 function MapActivity() {
+  useEffect(() => {
+    loadNet();
+  }, [])
+
+  const [networkData, setNetworkData] = useState([]);
+  const [stationData, setStationData] = useState([]);
+
+  const loadNet = async () => {
+    const net_data = await fetch(appSettings.METEONET_URL + 'execute.php?action=json&json_request=load_net');
+    const station_data = await fetch(appSettings.METEONET_URL + 'execute.php?action=json&json_request=load_last');
+    const json_net_data = await net_data.json();  
+    const json_station_data = await station_data.json();
+    setNetworkData(json_net_data.stations);
+    setStationData(json_station_data.stations);
+  }
+
   return (
     <div className="MapActivity">
       <div className="leaflet-container">
-      <MapContainer center={[41.7892, 15.7416]} zoom={11}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="/info">WeatherLab360</a> visualizer'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[41.7892, 15.7416]} icon={StationMarker}>
-          <Popup>
-            MARKER DI ESEMPIO
-          </Popup>
-          <Tooltip direction="left" offset={[-20, 0]} opacity={1} permanent>
-            <b>10C°</b>
-          </Tooltip>
-        </Marker>
-      </MapContainer>
+        <MapContainer center={[appSettings.MAP_CENTER_LAT, appSettings.MAP_CENTER_LON]} zoom={11}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="/info">WeatherLab360</a> visualizer'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {
+            Object.entries(networkData).map(([statSysName,station]) => (
+              <Marker position={[station.gps_long, station.gps_lat]} icon={StationMarker}>
+                <Popup>
+                  <b>{station.location}</b><br/>
+                  Orario: {stationData.length !== 0 && new Date(stationData[statSysName].data.timestamp * 1000).toLocaleTimeString()} <br/>
+                  Temperatura: {stationData.length !== 0 && stationData[statSysName].data.temp} C°<br/>
+                  Umidità: {stationData.length !== 0 && stationData[statSysName].data.rh} %<br/>
+                  Pressione: {stationData.length !== 0 && stationData[statSysName].data.press} hPa<br/>
+                  Vento: {stationData.length !== 0 && stationData[statSysName].data.wind_speed} km/h<br/>
+                  Pioggia (Anno): {stationData.length !== 0 && stationData[statSysName].data.rain_year} mm<br/>
+                  {//TODO: Cambiare nell'API meteonet il fuzionamento del valore rain così che ritorni la giornaliera
+                  }
+                  </Popup>
+                <Tooltip direction="left" offset={[-20, 0]} opacity={1} permanent>
+                  <b>{stationData.length !== 0 && stationData[statSysName].data.temp}C°</b>
+                </Tooltip>
+              </Marker>
+            ))
+          }
+        </MapContainer>
       </div>
     </div>
   );
---
+++ src/settings.js
@@ -0,0 +1,3 @@
+export const METEONET_URL = "http://www.meteogargano.org/meteo/meteonet/";
+export const MAP_CENTER_LAT = 41.80;
+export const MAP_CENTER_LON = 15.80;
